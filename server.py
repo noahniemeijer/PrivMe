0e7b558f10dfafd2ee1a1220fc5ba9dce043829c
tetie12345 <floristabak@gmail.com>
2024-11-10 01:14:08 +0100

    made some working server and client which can interact

---
+++ server.py
@@ -0,0 +1,26 @@
+import asyncio
+from websockets.asyncio.server import serve
+
+connectedSockets = []
+
+async def echo(websocket):
+    connectedSockets.append(websocket)
+    print(connectedSockets)
+
+    async for message in websocket:
+
+        for i in connectedSockets:
+            await i.send(message)
+            print(i, message)
+
+        print(message, websocket)
+
+
+async def main():
+    async with serve(echo, "localhost", 8765) as server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
